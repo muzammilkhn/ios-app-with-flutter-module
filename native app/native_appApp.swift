af5ba21243533403fa975b91e0fa0b15b3571c47
Muhammad Muzammil <muzammil.khan@techlogix.com>
2023-11-07 11:49:24 +0500

    Initial Commit

---
+++ native app/native_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_appApp.swift
+//  native app
+//
+//  Created by Sysop on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct native_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
